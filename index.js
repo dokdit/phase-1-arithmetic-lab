5a141a0826c3b1a67baca2301ec852b276e97010
AK <akueiminyang@cmail.carleton.ca>
2023-03-13 17:46:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1,num2;
+num1=31,num2=2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3,num4;
+num3 = 24,num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(6, 20, 17, 14, 5);
